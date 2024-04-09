0e7ae45db2f16e48260ca6ed30c324a627abef6d
Pranav <pranav.techiegeek@gmail.com>
2024-04-09 12:11:20 +0530

    ready

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
 
-def fetch_official_name(country):
-
+def send_name(name):
     # write your code here
-
     pass
 
 
-# Sample Outputs
-print(fetch_official_name('USA')) # Output: United States of America
-print(fetch_official_name('Brazil')) # Output: Federative Republic of Brazil
-print(fetch_official_name('France')) # Output: French Republic
+# Sample Tests
+# The function should return valid json response from API
+print(send_name('jack beyer'))
+print(send_name('scott manely'))
+print(send_name('tim dodd'))
