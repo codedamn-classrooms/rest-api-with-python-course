06d491fb9c6cb305baa3ec7b8eaf3c6878091fe5
Pranav <pranav.techiegeek@gmail.com>
2024-04-09 11:37:13 +0530

    ready

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+# send a response to the given API link from Instructions
+request = # Write your code here
+
+# fetch the json data of the response
+response =  # set the value of 'response' to contain the JSON data of the response
