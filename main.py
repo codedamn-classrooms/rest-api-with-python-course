7c48e372466dc4218a866738f37fa4b9ee3db3bd
Pranav <pranav.techiegeek@gmail.com>
2024-04-09 11:41:45 +0530

    ready

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 
-# send a response to the given API link from Instructions
+# send a response to the given API link to fetch details of Japan
 request = # Write your code here
 
-# fetch the json data of the response
-response =  # set the value of 'response' to contain the JSON data of the response
+response = request.json()
